70e479eba28b93798dbd1eb5aec0405d8cbbb9e7
WJurgiel <wjurgiel@student.agh.edu.pl>
2024-05-31 18:17:23 +0200

    Added patient information panel
    Now not only can you log in as a doctor but also as a patient.
    Informations panel in patientUI added

--- Patient.cs
+++ Patient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,56 @@ namespace Hospital
 {
     public partial class Patient : Form
     {
-        public Patient()
+        private string dbConnection;
+        private MySqlConnection mySqlConnection;
+        private int PatientID { get; set; } 
+        private string name, surname, phoneNumber, diagnose, doctorName, doctorSurname;
+
+   
+
+        public Patient(string dbCon, int patientID)
         {
             InitializeComponent();
+
+            dbConnection = dbCon;
+            mySqlConnection = new(dbCon);
+
+            PatientID = patientID;
+
+            CreateInformationsPanel();
+        }
+
+        private void CreateInformationsPanel()
+        {
+            mySqlConnection.Open();
+            string query = $"SELECT p.Imie, p.Nazwisko, p.Diagnoza, p.nr_bliskiego, d.Imie as Imie_Doktora, d.Nazwisko as Nazwisko_Doktora FROM pacjenci as p JOIN lekarze as d ON p.ID_Lekarza = d.ID WHERE p.ID = {PatientID}";
+            MySqlCommand command = new(query, mySqlConnection);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                name = reader.GetString("Imie");
+                surname = reader.GetString("Nazwisko");
+                phoneNumber = reader.GetString("nr_bliskiego");
+                diagnose = reader.GetString("Diagnoza");
+                doctorName = reader.GetString("Imie_Doktora");
+                doctorSurname = reader.GetString("Nazwisko_Doktora");
+
+            }
+            reader.Close();
+            mySqlConnection.Close();
+
+            PatientNameLabel.Text = name;
+            PatientSurnameLabel.Text = surname;
+            PatientDiagnoseLabel.Text = diagnose;
+            PatientRelativeNumberLabel.Text = phoneNumber;
+            PatientDoctorAsignedLabel.Text = $"{doctorName} {doctorSurname}";
+        }
+
+        private void LogoutButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Application.OpenForms["Form1"].Show();
         }
     }
 }
